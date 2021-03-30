2606eddeb4d6e9afa22e105c260e313250404963
rbryanben <rbryanben@gmail.com>
2021-03-30 22:58:54 +0200

    source files

--- Site/urls.py
+++ Site/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('',views.homePage,name="Home"), # Home-page
+    path('facilities/',views.facilitiesPage,name="Facilities"),  #Facilities
+    path('academics/',views.academicsPage,name="Academics"),  #Academics
+    path('gallery/',views.galleryPage,name="Gallery"), #Gallery
+    path('events/',views.eventsPage,name="Events"), #Events
+    path('news/',views.newsPage,name="News"), #News
+    path('admissions/',views.admissionsPage,name="Admissions"), #Admissions
 ] 
--- Site/views.py
+++ Site/views.py
@@ -4,3 +4,22 @@ from django.http import HttpResponse
 #home page 
 def homePage(request):
     return render(request,'Site/home/home.html')
+
+def facilitiesPage(request):
+    return render(request,'Site/facilities/facilities.html')
+
+
+def academicsPage(request):
+    return render(request,'Site/academics/academics.html')
+
+def galleryPage(request):
+    return render(request,"Site/gallery/gallery.html")
+
+def eventsPage(request):
+    return render(request,'Site/events/events.html')
+
+def newsPage(request):
+    return render(request,'Site/news/news.html')
+
+def admissionsPage(request):
+    return render(request,"Site/admissions/admissions.html")
